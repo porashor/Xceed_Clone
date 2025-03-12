d75f59dcb79eb5a6fcedcf4f4ada0c89307f4dee
Parashar Das <porashorbabu@gmail.com>
2025-03-12 23:21:53 +0600

    portfolio page has done

---
+++ src/Data/PortfolioData.jsx
@@ -0,0 +1,155 @@
+import p1 from "../assets/Portfolio/p1.jpg"
+import p2 from "../assets/Portfolio/p2.jpg"
+import p3 from "../assets/Portfolio/p3.jpg"
+import p4 from "../assets/Portfolio/p4.jpg"
+import p5 from "../assets/Portfolio/p5.jpg"
+import p6 from "../assets/Portfolio/p6.jpg"
+import p7 from "../assets/Portfolio/p7.jpg"
+import p8 from "../assets/Portfolio/p8.jpg"
+import p9 from "../assets/Portfolio/p9.jpg"
+import p10 from "../assets/Portfolio/p10.jpg"
+import p11 from "../assets/Portfolio/p11.jpg"
+import p12 from "../assets/Portfolio/p12.jpg"
+import p13 from "../assets/Portfolio/p13.jpg"
+import p14 from "../assets/Portfolio/p14.jpg"
+import p15 from "../assets/Portfolio/p15.jpg"
+import p16 from "../assets/Portfolio/p16.jpg"
+import p17 from "../assets/Portfolio/p17.jpg"
+import p18 from "../assets/Portfolio/p18.jpg"
+import p19 from "../assets/Portfolio/p19.jpg"
+import p20 from "../assets/Portfolio/p20.jpg"
+import p21 from "../assets/Portfolio/p21.jpg"
+
+
+
+
+
+
+export const portfoliodata = [
+    {
+        name: "Arohi Group",
+        details: "Manufacturer & Exporters",
+        links: "#",
+        image: p1
+    },
+    {
+        name: "Ayub Trade Centre",
+        details: "Ayub Trade center is a complete Commercial Building",
+        links: "#",
+        image: p2
+    },
+    {
+        name: "Canvas Of Color",
+        details: "Wedding, Cinematography, Pre or Post Wedding",
+        links: "#",
+        image: p3
+    },
+    {
+        name: "CBIFT",
+        details: "Chattogram BGMEA Institute of Fashion & Technology",
+        links: "#",
+        image: p4
+    },
+    {
+        name: "EY Immigration",
+        details: "Professional consulting firm",
+        links: "#",
+        image: p5
+    },
+    {
+        name: "H.S. Traders",
+        details: "Import based business firm",
+        links: "#",
+        image: p6
+    },
+    {
+        name: "immt bd",
+        details: "INSTITUTE OF MANAGEMENT & MEDICAL TECHNOLOGY",
+        links: "#",
+        image: p7
+    },
+    {
+        name: "infiniteslbd",
+        details: "SHIPPING & LOGISTICS",
+        links: "#",
+        image: p8
+    },
+    {
+        name: "KEN",
+        details: "ACCESSORIES INDUSTRY",
+        links: "#",
+        image: p9
+    },
+    {
+        name: "Sea Gold Shipping",
+        details: "Seafarer’s recruitment company",
+        links: "#",
+        image: p10
+    },
+    {
+        name: "The Avenue",
+        details: "Hotel & Suites",
+        links: "#",
+        image: p11
+    },
+    {
+        name: "Western Marine Shipyard Ltd",
+        details: "shipbuilder",
+        links: "#",
+        image: p12
+    },
+    {
+        name: "CIDER International School",
+        details: "School Website design and development",
+        links: "#",
+        image: p13
+    },
+    {
+        name: "H M Steel & Industry limited",
+        details: "Web design and Development",
+        links: "#",
+        image: p14
+    },
+    {
+        name: "FMC Dockyard",
+        details: "Web design and Development",
+        links: "#",
+        image: p15
+    },
+    {
+        name: "Western Marine Services Ltd",
+        details: "Technical expertise in marine sector",
+        links: "#",
+        image: p16
+    },
+    {
+        name: "ZARANUR",
+        details: "Marine machinery and spares parts",
+        links: "#",
+        image: p17
+    },
+    {
+        name: "ZENITH",
+        details: "TEST & INSPECTION SERVICES",
+        links: "#",
+        image: p18
+    },
+    {
+        name: "Primex Logistics International",
+        details: "Web Design and Development",
+        links: "#",
+        image: p19
+    },
+    {
+        name: "Sinoben Group",
+        details: "Web Design and development",
+        links: "#",
+        image: p20
+    },
+    {
+        name: "Dr Ariful Profile Website",
+        details: "Web Design and development",
+        links: "#",
+        image: p21
+    },
+]
--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
+import { portfoliodata } from '../Data/PortfolioData'
+import Getstart from '../Components/Getstart'
+
+
 
 const Portfolio = () => {
   return (
-    <div>
-      this is the portfolio page 
+    <div className=''>
+      <Helmet>
+        <title>Work - Website Design and Software Development Company</title>
+      </Helmet>
+      {/* header section  */}
+      <div className='flex w-[80%] mx-auto flex-col items-center justify-center gap-5 py-10'>
+        <h1 className='text-3xl lg:text-4xl font-bold text-yellow-600'>Our Portfolio</h1>
+        <div className='text-xl lg:text-2xl text-center'>We have created, taken care of, and advertised hundreds of websites in the past 16 years. You can view our work by checking out the links below.</div>
+      </div>
+      {/* main portfolio section  */}
+      <div className='w-[90%] mx-auto py-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-between gap-4 '>
+        {portfoliodata.map((item, index)=>(
+          <a href={item.links} className=' flex flex-col items-start justify-start gap-4 border-[3px] border-slate-300 rounded-md p-3 shadow-xl' key={index}>
+            {/* image section  */}
+            <img src={item.image} className='w-full ' alt="" />
+            {/* title section  */}
+            <h1 className='text-2xl text-center font-bold'>{item.name}</h1>
+            {/* details section  */}
+            <p className='uppercase text-center text-xl text-slate-400'>{item.details}</p>
+          </a>
+        ))}
+      </div>
+      {/* get start  */}
+      <Getstart/>
     </div>
   )
 }
